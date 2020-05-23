cefb65928b62bba5e35b180315f1b623ba510a12
2GEasy <hagun119@gmail.com>
2020-05-24 03:12:10 +0900

    Category Design Modify

--- src/customer/component/Category.js
+++ src/customer/component/Category.js
@@ -1,15 +1,40 @@
 import React,{Component} from 'react';
-import { Grid } from '@material-ui/core';
+import { Grid, TextareaAutosize, Container } from '@material-ui/core';
 import Paper from '@material-ui/core/Paper';
 import CardView from './CardView';
+import Card from '@material-ui/core/Card';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import Hidden from '@material-ui/core/Hidden';
+import Typography from '@material-ui/core/Typography';
 import img1 from '../img/normaldsr.jpg';
 import img2 from '../img/healthdsr.jpg';
 import img3 from '../img/dndsr.gif';
 import img4 from '../img/premiumdsr.jpg';
 import {Link} from 'react-router-dom';
+import PropTypes from 'prop-types';
+import { withStyles,makeStyles } from '@material-ui/core/styles';
 
-class Category extends Component {
-    render() {
+const useStyles = makeStyles({
+    card:{
+        MuiPaper:{
+            margin:'0 auto',
+        },
+        width:'28vw',height:'28vh',margin:'0',
+        
+    },
+    cardMedia:{
+        width:'28vw',height:'20vh',margin:'0'
+    },
+    gridItem:{
+        margin:0,
+        padding:10
+    }
+  });
+
+export default function Category(props) {
+    
         const style = {
             root: {
                 marginTop: '20px',
@@ -21,40 +46,91 @@ class Category extends Component {
                 textDecoration: 'none'
             }
         }
+        const classes = useStyles();
         return (
             <>
-            <div style={style.root}>
-            <Grid container spacing={1} style={{align:'center'}}>
-                    <Grid item xs={3}></Grid>
-                    <Grid item xs={3}>
+                <Container maxWidth="md">
+                <Grid container direction="rows" align="center">
+                    <Grid item xs={12} sm={6} className={classes.gridItem}>
                         <Link to="/customer/category/normal" style={style.link}>
-                            <CardView img={img1} width="100%" height="120" title="일반 도시락" content="맛있는 일반식"/>
+                        <Card className={classes.card} >
+                            <Hidden>
+                                <CardMedia className={classes.cardMedia} image={img1} />
+                            </Hidden>
+                            <div className={classes.cardDetails}>
+                                <CardContent>
+                                <Typography component="h5" variant="h6">
+                                    일반 도시락
+                                </Typography>
+                                <Typography variant="subtitle1" color="textSecondary">
+                                맛있는 일반식
+                                </Typography>
+                                </CardContent>
+                            </div>
+                        </Card>
+                            {/* <CardView img={img1} width="100%" height="120" title="일반 도시락" content="맛있는 일반식"/> */}
                         </Link>
                     </Grid>
-                    <Grid item xs={3}>\
-                    <Link to="/customer/category/health" style={style.link}>
-                        <CardView img={img2} width="100%" height="120" title="건강 도시락" content="관리를 위한 건강식"/>
-                    </Link>
+                    <Grid item xs={12} sm={6} className={classes.gridItem}>
+                        <Link to="/customer/category/health" style={style.link}>
+                            <Card className={classes.card}>
+                                <Hidden>
+                                    <CardMedia className={classes.cardMedia} image={img2} />
+                                </Hidden>
+                                <div className={classes.cardDetails}>
+                                    <CardContent>
+                                    <Typography component="h5" variant="h6">
+                                    건강 도시락
+                                    </Typography>
+                                    <Typography variant="subtitle1" color="textSecondary">
+                                    관리를 위한 건강식
+                                    </Typography>
+                                    </CardContent>
+                                </div>
+                            </Card>
+                        </Link>
                     </Grid>
-                    <Grid item xs={3}></Grid>
-            </Grid>
-            <Grid container spacing={1}>
-            <Grid item xs={3}></Grid>
-                <Grid item xs={3}>
-                <Link to="/customer/category/lowsalt" style={style.link}>
-                    <CardView img={img3} width="100%" height="120" title="특별식 도시락" content="당뇨 식단, 저염식"/>
-                </Link>
+                    <Grid item xs={12} sm={6} className={classes.gridItem}>
+                        <Link to="/customer/category/lowsalt" style={style.link}>
+                            <Card className={classes.card} >
+                                <Hidden>
+                                    <CardMedia className={classes.cardMedia} image={img3}  />
+                                </Hidden>
+                                <div className={classes.cardDetails}>
+                                    <CardContent>
+                                    <Typography component="h5" variant="h6">
+                                    특별식 도시락
+                                    </Typography>
+                                    <Typography variant="subtitle1" color="textSecondary">
+                                    당뇨 식단, 저염식
+                                    </Typography>
+                                    </CardContent>
+                                </div>
+                            </Card>
+                        </Link>
+                    </Grid>
+                    <Grid item xs={12} sm={6} className={classes.gridItem}>
+                        <Link to={`/customer/category/premium`} style={style.link}>
+                            <Card className={classes.card} >
+                                <Hidden>
+                                    <CardMedia className={classes.cardMedia} image={img4}  />
+                                </Hidden>
+                                <div className={classes.cardDetails}>
+                                    <CardContent>
+                                    <Typography component="h5" variant="h6">
+                                    프리미엄 도시락
+                                    </Typography>
+                                    <Typography variant="subtitle1" color="textSecondary">
+                                    특별한 날과 특별한 나를 위한 프리미엄 도시락
+                                    </Typography>
+                                    </CardContent>
+                                </div>
+                            </Card>
+                        </Link>
+                    </Grid>
                 </Grid>
-                <Grid item xs={3}>
-                <Link to={`/customer/category/premium`} style={style.link}>
-                    <CardView img={img4} width="100%" height="120" title="프리미엄 도시락" content="특별한 날과 특별한 나를 위한 프리미엄 도시락"/>
-                </Link>
-                </Grid>
-                <Grid item xs={3}></Grid>
-            </Grid>
-            </div>
+                </Container>
             </>
         );
-    }
+    
 }
-export default Category;
